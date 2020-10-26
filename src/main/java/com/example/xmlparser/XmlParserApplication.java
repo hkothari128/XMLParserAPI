5a4453ac3f1718d97a4890a18b1597647782948e
rohitpal <rohitpal@localhost>
2020-10-26 16:36:48 +0530

    working for creating java object from xml file

--- src/main/java/com/example/xmlparser/XmlParserApplication.java
+++ src/main/java/com/example/xmlparser/XmlParserApplication.java
@@ -3,11 +3,15 @@ package com.example.xmlparser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.xmlparser.jaxb.XmlToObject;
+
 @SpringBootApplication
 public class XmlParserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XmlParserApplication.class, args);
+		XmlToObject obj = new XmlToObject();
+		obj.unmarshalling();
 	}
 
 }
